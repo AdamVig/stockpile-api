347ef77e25b65855e901df112cfc2e8d48c21abf
Adam Vigneaux <adamvigneaux@gmail.com>
2017-01-29 03:08:28 -0500

    Add database URL to environment variables
    This allows remote access to the database instead of assuming that it
    will always be available on localhost.

--- services/db.js
+++ services/db.js
@@ -4,7 +4,7 @@ const restify = require('restify')
 const db = module.exports = require('knex')({
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
+    host: process.env.DB_URL,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
