138c04cc128a9ac4a50e3a19076809c0c3c84438
Adam Vigneaux <adamvigneaux@gmail.com>
2017-02-16 20:59:41 -0500

    Add item get and getAll controllers

--- controllers/item.js
+++ controllers/item.js
@@ -1,9 +1,15 @@
+const db = require('../services/db')
+
 module.exports.getAll = (req, res, next) => {
-  res.send({})
+  return db.getAll('item', req.user.organizationID)
+    .then(items => res.send({items}))
+    .catch(next)
 }
 
 module.exports.get = (req, res, next) => {
-  res.send({})
+  return db.get('item', 'itemID', req.params.itemID, req.user.organizationID)
+    .then(item => res.send(item))
+    .catch(next)
 }
 
 module.exports.create = (req, res, next) => {
