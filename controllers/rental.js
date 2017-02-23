81350ab35543d73cd10205d4e7241c0c5f6af51e
Adam Vigneaux <adamvigneaux@gmail.com>
2017-02-22 23:04:59 -0500

    Change from rentalID to tag on rental endpoints
    Closes #94.

--- controllers/rental.js
+++ controllers/rental.js
@@ -3,15 +3,25 @@ const endpoint = require('../services/endpoint')
 
 const rental = module.exports
 
-endpoint.addAllMethods(rental, 'rental', 'rentalID')
+// Join rental with item to get the item's tag
+rental.withTag = (queryBuilder) => {
+  return queryBuilder
+    .select('rental.*')
+    .leftJoin('item', 'rental.itemID', 'item.itemID')
+    .select('item.tag')
+}
+
+endpoint.addAllMethods(rental, 'rental', 'tag')
+rental.getAll = endpoint.getAll('rental', rental.withTag)
+rental.get = endpoint.get('rental', 'tag', rental.withTag)
 
 rental.mount = app => {
   app.get({name: 'get all rentals', path: 'rental'}, auth.verify, rental.getAll)
-  app.get({name: 'get rental', path: 'rental/:rentalID'},
+  app.get({name: 'get rental', path: 'rental/:tag'},
           auth.verify, rental.get)
   app.put({name: 'create rental', path: 'rental'}, auth.verify, rental.create)
-  app.put({name: 'update rental', path: 'rental/:rentalID'},
+  app.put({name: 'update rental', path: 'rental/:tag'},
           auth.verify, rental.update)
-  app.del({name: 'delete rental', path: 'rental/:rentalID'},
+  app.del({name: 'delete rental', path: 'rental/:tag'},
           auth.verify, rental.delete)
 }
