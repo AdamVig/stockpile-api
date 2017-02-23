48b5275b69b9bcf0bbee557e94a38a658d07dd98
Adam Vigneaux <adamvigneaux@gmail.com>
2017-02-22 23:05:30 -0500

    Add full data to item response
    Closes #78.

--- controllers/item.js
+++ controllers/item.js
@@ -3,7 +3,31 @@ const endpoint = require('../services/endpoint')
 
 const item = module.exports
 
+// Join item with all of its fields
+item.withFields = (queryBuilder) => {
+  return queryBuilder
+    .select('item.*')
+
+  // Status
+    .leftJoin('status', 'item.statusID', 'status.statusID')
+    .select('status.name as status')
+
+  // Brand
+    .leftJoin('brand', 'item.brandID', 'brand.brandID')
+    .select('brand.name as brand')
+
+  // Model
+    .leftJoin('model', 'item.modelID', 'model.modelID')
+    .select('model.name as model')
+
+  // Category
+    .leftJoin('category', 'item.categoryID', 'category.categoryID')
+    .select('category.name as category')
+}
+
 endpoint.addAllMethods(item, 'item', 'tag')
+item.getAll = endpoint.getAll('item', item.withFields)
+item.get = endpoint.get('item', 'tag', item.withFields)
 
 item.mount = app => {
   app.get({name: 'get all items', path: 'item'}, auth.verify, item.getAll)
