10e7609b531780717d41c50e2fd39762d6dd3349
Adam Vigneaux <adamvigneaux@gmail.com>
2017-03-19 11:47:41 -0400

    Add docs for main controller

--- controllers/main.js
+++ controllers/main.js
@@ -3,6 +3,17 @@ const endpoint = require('../services/endpoint')
 const main = module.exports
 
 main.mount = app => {
+  /**
+   * @api {get} / Get main
+   * @apiName GetMain
+   * @apiGroup Main
+   *
+   * @apiDescription This is the entry point for the API. Using HAL relations,
+   * the client can navigate the whole API based on the response from this
+   * endpoint.
+   *
+   * @apiSuccess (200) empty No response body
+   */
   app.get({name: 'main', path: '/'}, main.get)
 }
 
