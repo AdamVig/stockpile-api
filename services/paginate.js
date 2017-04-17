62c5a0f076e11f124813362bd4f62acb4adff963
Adam Vigneaux <adamvigneaux@gmail.com>
2017-04-17 13:44:34 -0400

    Add method to build pagination links

--- services/paginate.js
+++ services/paginate.js
@@ -1,11 +1,67 @@
+const db = require('./db')
+
+/**
+ * Create links for adding to the `Link` header
+ * @param {string} path Path of requested endpoint
+ * @param {number} limit Maximum number of rows returned
+ * @param {number} [offset=0] How many rows to discard before returning the rest
+ * @param {number} [total=0] Total number of rows
+ * @return {object} Contains links to first, last, previous (maybe),
+ *   next (maybe) pages of results
+ */
+module.exports.createLinks = (path, limit, offset = 0, total = 0) => {
+  const links = {}
+  const base = `${path}?limit=${limit}&offset=`
+
+  // Offset of zero for first page
+  links.first = `${base}${0}`
+
+  // Add prev link if beyond first page
+  if (offset > limit) {
+    // Use `max` to prevent offset from going negative
+    links.previous = `${base}${Math.max(0, offset - limit)}`
+  }
+
+  // Add next link if there is a next page
+  if ((offset + limit) <= total) {
+    // Use `min` to prevent offset from going above total
+    links.next = `${base}${Math.min(total, offset + limit)}`
+  }
+
+  // If total % limit !== 0, use remainder as diff
+  // If total % limit === 0, last page is same as last "next" link
+  // This prevents overlapping items between the two last pages
+  const lastPageDiff = (total % limit) || limit
+  // Use `max` to prevent from going negative when total is zero
+  links.last = `${base}${Math.max(0, total - lastPageDiff)}`
+
+  return links
+}
+
+/**
+ * Add links to the `Link` header for pagination
+ * @param {object} req HTTP request
+ * @param {object} res HTTP response
+ * @param {string} tableName Name of a database table
+ * @return {Promise} Resolved when done adding links
+ */
+module.exports.addLinks = (req, res, tableName) => {
+  return db.countRows(tableName, req.user.organizationID)
+    .then(({count}) => {
+      // Generate and add links to response headers
+      const links = module.exports.createLinks(req.path(), req.params.limit,
+                                               req.params.offset, count)
+      res.links(links)
+    })
+}
+
 /**
  * Add `limit` and `offset` to a database query with values from
  * request parameters
  * @param {object} queryBuilder Knex query builder
  * @param {object} req HTTP request
- * @param {object} res HTTP response
  */
-module.exports.paginateQuery = (queryBuilder, req, res) => {
+module.exports.paginateQuery = (queryBuilder, req) => {
   if (req && req.params.limit) {
     queryBuilder
       .limit(Number.parseInt(req.params.limit, 10))
--- test/paginate.js
+++ test/paginate.js
@@ -2,14 +2,26 @@ const sinon = require('sinon')
 const test = require('ava')
 
 const fixt = require('./fixtures/paginate')
+const knex = require('./fixtures/knex-instance')
 const paginate = require('../services/paginate')
+
+test.before('Prepare database', async t => {
+  fixt.table = knex.randomizeTableName(fixt.table)
+  await knex.schema.createTable(fixt.table, table => {
+    table.string('name')
+    table.integer('organizationID')
+  })
+
+  // Insert multiple rows
+  await knex(fixt.table).insert(fixt.rows)
+})
 
 test('Paginate query', t => {
   const queryBuilder = {
     limit: sinon.stub(),
     offset: sinon.spy()
   }
-  paginate(queryBuilder, fixt.req)
+  paginate.paginateQuery(queryBuilder, fixt.req)
   t.true(queryBuilder.limit.calledOnce, 'limit is called')
   t.true(queryBuilder.offset.calledOnce, 'offset is called')
 })
@@ -19,7 +31,7 @@ test('Paginate query with only limit', t => {
     limit: sinon.spy(),
     offset: sinon.spy()
   }
-  paginate(queryBuilder, fixt.reqOnlyLimit)
+  paginate.paginateQuery(queryBuilder, fixt.reqOnlyLimit)
   t.true(queryBuilder.limit.calledOnce, 'limit is called')
   t.false(queryBuilder.offset.called, 'offset is not called')
 })
@@ -29,7 +41,7 @@ test('Paginate query with only offset', t => {
     limit: sinon.spy(),
     offset: sinon.spy()
   }
-  paginate(queryBuilder, fixt.reqOnlyOffset)
+  paginate.paginateQuery(queryBuilder, fixt.reqOnlyOffset)
   t.true(queryBuilder.offset.calledOnce, 'offset is called')
   t.false(queryBuilder.limit.called, 'limit is not called')
 })
@@ -39,7 +51,57 @@ test('Paginate query with no parameters', t => {
     limit: sinon.spy(),
     offset: sinon.spy()
   }
-  paginate(queryBuilder, null)
+  paginate.paginateQuery(queryBuilder, null)
   t.false(queryBuilder.limit.called, 'limit is not called')
   t.false(queryBuilder.offset.called, 'offset is not called')
+})
+
+test('Add links', async t => {
+  const req = {
+    params: fixt.addLinksParams,
+    path: sinon.stub().returns(fixt.path),
+    user: fixt.user
+  }
+  const res = {
+    links: sinon.spy()
+  }
+  await paginate.addLinks(req, res, fixt.table)
+  t.true(res.links.calledOnce, 'adds links to response')
+})
+
+test('Create links with no previous or next pages', t => {
+  const links = paginate.createLinks(fixt.path, fixt.paramsNoPrevNext.limit,
+                                     fixt.paramsNoPrevNext.offset,
+                                     fixt.paramsNoPrevNext.count)
+  t.deepEqual(links, fixt.linksNoPrevNextExpected, 'creates links correctly')
+})
+
+test('Create "last" link with odd total', t => {
+  const links = paginate.createLinks(fixt.path, fixt.paramsOddTotal.limit,
+                                     fixt.paramsOddTotal.offset,
+                                     fixt.paramsOddTotal.count)
+  t.is(links.last, fixt.lastLinkOddTotalExpected, 'creates links correctly')
+})
+
+test('Create links with previous but not next page', t => {
+  const links = paginate.createLinks(fixt.path, fixt.paramsPrev.limit,
+                                     fixt.paramsPrev.offset,
+                                     fixt.paramsPrev.count)
+  t.deepEqual(links, fixt.linksPrevExpected, 'creates links correctly')
+})
+
+test('Create links with next but not previous page', t => {
+  const links = paginate.createLinks(fixt.path, fixt.paramsNext.limit,
+                                     fixt.paramsNext.offset,
+                                     fixt.paramsNext.count)
+  t.deepEqual(links, fixt.linksNextExpected, 'creates links correctly')
+})
+
+test('Create links with no offset or total values', t => {
+  const links = paginate.createLinks(fixt.path, fixt.noOffsetTotalLimit)
+  t.deepEqual(links, fixt.linksNoOffsetTotalExpected, 'creates links correctly')
+})
+
+test.after.always('Clean up database', async t => {
+  await knex.schema.dropTable(fixt.table)
 })
