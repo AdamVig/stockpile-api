f6e7c8c1a183692cbc28b4d7ee158c310696b089
Adam Vigneaux <adamvigneaux@gmail.com>
2017-07-28 16:23:20 -0400

    Set first user to admin and include created IDs in response
    The first user in the organization should be an administrator so that he or she can add and administer other users. Including the created IDs in the response is helpful for then retrieving those entities.

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -20,13 +20,16 @@ subscription.mount = app => {
    * @apiDescription Create a new Stockpile subscription for an organization. First, using a Stripe token, creates a
    * Stripe customer with a subscription to Stockpile. Second, creates an organization using the request body and the
    * created Stripe customer's unique identifier. Lastly, creates a user using the request body and the created
-   * organization's ID.
+   * organization's ID. This user, the first in the organization, will be an administrator by default.
    *
    * @apiParam {Object} token Stripe token identifying payment information
    * @apiParam {Object} organization Contains name and email
    * @apiParam {Object} user Contains `firstName`, `lastName`, `email`, and plaintext `password`
    *
    * @apiSuccess (201) {String} message Descriptive message
+   * @apiSuccess (201) {String} organizationID
+   * @apiSuccess (201) {String} stripeCustomerID
+   * @apiSuccess (201) {String} userID
    */
   app.post({ name: 'subscription', path: 'subscription' }, subscription.subscription)
 }
@@ -50,15 +53,28 @@ subscription.subscription = (req, res, next) => {
         if (organization) {
           user.organizationID = organization.organizationID
         }
-        return auth.hashPassword(user.password)
+
+        // Set first user in organization to 'Admin' role
+        user.roleID = 1
+
+        const creatingUser = auth.hashPassword(user.password)
           .then(hash => {
             user.password = hash
             return db.create('user', 'userID', user)
           })
+
+        return Promise.all([
+          organization.organizationID,
+          organization.stripeCustomerID,
+          creatingUser
+        ])
       })
-      .then(user => {
+      .then(([organizationID, stripeCustomerID, user]) => {
         return res.send(201, {
-          message: 'Subscription created'
+          message: 'Subscription created',
+          organizationID,
+          stripeCustomerID,
+          userID: user.userID
         })
       })
       .catch(err => {
