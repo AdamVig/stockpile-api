c52aa5f6f2317a24a80978e694a2f970c1f4cbbd
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-10 16:04:39 -0400

    Allow custom fields to have multiple categories

--- controllers/custom-field.js
+++ controllers/custom-field.js
@@ -1,5 +1,8 @@
+const restify = require('restify')
+
 const auth = require('./auth')
 const checkSubscription = require('../services/check-subscription')
+const db = require('../services/db')
 const endpoint = require('../services/endpoint')
 
 const customField = module.exports
@@ -11,6 +14,67 @@ const messages = {
 
 endpoint.addAllMethods(customField, 'customField', 'customFieldID', messages)
 
+// Add category name
+customField.withCategoryName = (req, queryBuilder) => {
+  return queryBuilder
+    .join('category', 'customFieldCategory.categoryID', 'category.categoryID')
+}
+const categoryMessages = {
+  create: 'Added category to custom field',
+  createPlural: 'Added categories to custom field',
+  missing: 'Custom field has no categories'
+}
+customField.getCategories = endpoint.get('customFieldCategory', 'customFieldID', {
+  modify: customField.withCategoryName,
+  messages: categoryMessages,
+  hasOrganizationID: false
+})
+
+customField.updateCategories = (req, res, next) => {
+  if (req.body && req.body.categories) {
+    // Convert incoming categories to table schema
+    const customFieldCategories = req.body.categories.map(category => {
+      return {
+        customFieldID: req.params.customFieldID,
+        categoryID: category.categoryID
+      }
+    })
+
+    return db.transaction(trx => {
+      // Remove all existing categories for this custom field
+      return trx('customFieldCategory').where('customFieldID', req.params.customFieldID).del()
+        .then(() => {
+          // Add all new categories
+          return trx('customFieldCategory').insert(customFieldCategories)
+        }).then(() => {
+          // Get all categories for this field
+          return trx('customFieldCategory')
+            .where('customFieldID', req.params.customFieldID)
+            .modify(customField.withCategoryName.bind(null, req))
+        })
+    }).then((rows) => {
+      let message = categoryMessages.create
+      if (req.body.categories.length > 1) {
+        message = categoryMessages.createPlural
+      }
+      res.send({
+        message,
+        categories: rows
+      })
+      return next()
+    }).catch(err => {
+      // Return useful error when custom field does not exist
+      if (err.code === 'ER_NO_REFERENCED_ROW_2') {
+        return next(new restify.BadRequestError('Custom field does not exist'))
+      } else {
+        return next(err)
+      }
+    })
+  } else {
+    return next(new restify.BadRequestError('Missing list of categories'))
+  }
+}
+
 customField.mount = app => {
   /**
    * @apiDefine CustomFieldResponse
@@ -19,8 +83,7 @@ customField.mount = app => {
    * {
    *   "customFieldID": 0,
    *   "organizationID": 0,
-   *   "name": "",
-   *   "categoryID": 0
+   *   "name": ""
    * }
    */
 
@@ -69,8 +132,6 @@ customField.mount = app => {
    *   `categoryID` value).
    *
    * @apiParam {String{0...255}} name Name of custom field, **must be unique**
-   * @apiParam {Number} [categoryID] ID of a category to associate this field
-   *   with, meaning that the field will only apply to items in this category
    * @apiParam {Number} [organizationID] ID of organization (automatically taken
    *   from token, but can be overridden)
    *
@@ -84,8 +145,6 @@ customField.mount = app => {
    * @apiPermission User
    *
    * @apiParam {String{0...255}} [name] Name of custom field
-   * @apiParam {Number} [categoryID] ID of a category to associate this field
-   *   with, meaning that the field will only apply to items in this category
    * @apiParam {Number} [organizationID] ID of organization (automatically taken
    *   from token, but can be overridden)
    *
@@ -106,4 +165,49 @@ customField.mount = app => {
    */
   app.del({name: 'delete custom field', path: 'custom-field/:customFieldID'}, auth.verify, checkSubscription,
     customField.delete)
+  /**
+   * @api {get} /custom-field/:customFieldID/categories Get all categories of a custom field
+   * @apiName GetCustomFieldCategories
+   * @apiGroup CustomField
+   * @apiPermission User
+   *
+   * @apiExample {json} Response Format
+   * {
+   *   results: [
+   *     {
+   *       "categoryID": 0,
+   *       "customFieldID": 0,
+   *       "name": "",
+   *       "organizationID": 0
+   *     }
+   *   ]
+   * }
+   */
+  app.get({name: 'get custom field categories', path: 'custom-field/:customFieldID/categories'}, auth.verify,
+    customField.getCategories)
+  /**
+   * @api {put} /custom-field/:customFieldID/:customFieldID/categories Update categories of a custom field
+   * @apiName UpdateCustomFieldCategories
+   * @apiGroup CustomField
+   * @apiPermission User
+   *
+   * @apiParam {Object[]} [categories] List of category entities that this field belongs in. The field will only apply
+   *   to items in this category
+   * @apiParam {Number} categories.categoryID ID of category
+   * @apiParam {String} categories.name Name of category
+   * @apiParam {Number} categories.organizationID Organization that category belongs to
+   *
+   * @apiExample {json} Response Format
+   * {
+   *   "categories": [
+   *     "categoryID": 0,
+   *     "customFieldID": 0,
+   *     "name": "",
+   *     "organizationID": 0
+   *   ],
+   *   "message": ""
+   * }
+   */
+  app.put({name: 'update custom field categories', path: 'custom-field/:customFieldID/categories'}, auth.verify,
+    checkSubscription, customField.updateCategories)
 }
---
+++ test/custom-field.js
@@ -0,0 +1,80 @@
+const restify = require('restify')
+const sinon = require('sinon')
+const test = require('ava')
+
+const customFieldController = require('../controllers/custom-field')
+const fixt = require('./fixtures/custom-field')
+const knex = require('./fixtures/knex-instance')
+
+test.before(async t => {
+  // Create an organization
+  const [organizationID] = await knex('organization').insert(fixt.organization)
+  fixt.organization.organizationID = organizationID
+
+  // Add organizationID to all categories
+  fixt.categories = fixt.categories.map(category => {
+    category.organizationID = organizationID
+    return category
+  })
+
+  // Create categories
+  for (const category of fixt.categories) {
+    const [categoryID] = await knex('category').insert(category)
+    category.categoryID = categoryID
+  }
+
+  // Create custom fields
+  fixt.customFields[0].organizationID = organizationID
+  fixt.customFields[1].organizationID = organizationID
+  fixt.updateCategories.req.params.customFieldID = await knex('customField').insert(fixt.customFields[0])
+  fixt.addMissingCategory.req.params.customFieldID = await knex('customField').insert(fixt.customFields[1])
+
+  // Add categories to requests
+  fixt.updateCategoriesMissingField.req.body.categories = fixt.categories
+  fixt.updateCategories.req.body.categories = fixt.categories
+})
+
+test('With category name', t => {
+  const queryBuilder = {
+    join: sinon.stub().returnsThis(),
+    select: sinon.stub().returnsThis(),
+    where: sinon.stub().returnsThis()
+  }
+  const result = customFieldController.withCategoryName(null, queryBuilder)
+  t.true(result === queryBuilder, 'returns query builder')
+})
+
+test('Update categories with no body', async t => {
+  const next = sinon.spy()
+  await customFieldController.updateCategories(fixt.updateCategoriesNoBody.req, null, next)
+  t.true(next.calledWithMatch(sinon.match.instanceOf(restify.BadRequestError)), 'throws bad request error')
+})
+
+test('Update categories on nonexistent custom field', async t => {
+  const next = sinon.spy()
+  await customFieldController.updateCategories(fixt.updateCategoriesMissingField.req, null, next)
+  t.true(next.calledWithMatch(sinon.match.instanceOf(restify.BadRequestError)), 'throws bad request error')
+})
+
+test('Add nonexistent category to custom field', async t => {
+  const next = sinon.spy()
+  await customFieldController.updateCategories(fixt.addMissingCategory.req, null, next)
+  t.true(next.calledWithMatch(sinon.match.instanceOf(restify.BadRequestError)), 'throws bad request error')
+})
+
+test('Update categories on custom field', async t => {
+  const res = {
+    send: sinon.spy()
+  }
+  const next = sinon.spy()
+  await customFieldController.updateCategories(fixt.updateCategories.req, res, next)
+  t.true(res.send.called, 'sends response')
+  t.true(next.called, 'calls next handler')
+  t.false(next.calledWithMatch(sinon.match.instanceOf(Error)), 'no errors')
+})
+
+test.after.always(async t => {
+  await knex('customField').where('organizationID', fixt.organization.organizationID).del()
+  await knex('category').where('organizationID', fixt.organization.organizationID).del()
+  await knex('organization').where('organizationID', fixt.organization.organizationID).del()
+})
