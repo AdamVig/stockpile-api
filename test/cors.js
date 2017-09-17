f12e66f30d33304e26d1678de23071adbe59c0c3
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-17 19:38:42 -0400

    Fix CORS test

--- test/cors.js
+++ test/cors.js
@@ -13,8 +13,8 @@ test('Sets headers', t => {
   const next = sinon.spy()
   cors.handle(req, res, next)
   t.true(res.header.calledWith('Access-Control-Allow-Origin') &&
-    res.header.calledWith('Access-Control-Allow-Headers'),
+    res.header.calledWith('Access-Control-Allow-Headers') &&
     res.header.calledWith('Access-Control-Allow-Methods'),
-    'sets headers')
+  'sets headers')
   t.true(next.called, 'calls next handler')
 })
