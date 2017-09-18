51249bcd465094fd97a892069b2c6bc0fa92e794
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-18 10:33:16 -0400

    Remove sensitive information from logs
    Change default log level from debug to info
    
    Add test for hiding password from logs
    
    Set log level per environment
    
    Hide password from logs

--- test/log.js
+++ test/log.js
@@ -8,6 +8,28 @@ test('Log exports an object', t => {
   t.true(typeof log === 'object')
 })
 
+const logLevel = {
+  info: 30,
+  debug: 20
+}
+
+test('Sets default log level correctly', t => {
+  t.true(log.streams.every(stream => stream.level === logLevel.info), 'all streams have info level')
+})
+
+test('Sets log level correctly in dev environment', t => {
+  const previousNodeEnv = process.env.NODE_ENV
+
+  process.env.NODE_ENV = 'DEVELOPMENT'
+  // Remove cached module so it instantiates itself again
+  delete require.cache[require.resolve('../services/log')]
+  const logDev = require('../services/log')
+
+  t.true(logDev.streams.every(stream => stream.level === logLevel.debug), 'all streams have debug level')
+
+  process.env.NODE_ENV = previousNodeEnv
+})
+
 test('Logs request', t => {
   const req = {
     log: {
@@ -50,6 +72,21 @@ test('Logs request with body', t => {
   t.true(req.log.info.calledWith({req}, 'request'), 'logs request')
   t.true(req.log.debug.calledWith({body: req.body}, 'request body'), 'logs body')
   t.false(req.log.debug.calledWith({params: undefined}, 'request parameters'), 'does not log params')
+  t.true(next.called, 'calls next handler')
+})
+
+test('Redacts password from request body', t => {
+  const req = {
+    body: fixt.redactsPassword.body,
+    log: {
+      debug: sinon.spy(),
+      info: sinon.spy()
+    }
+  }
+  const next = sinon.spy()
+  log.onRequest(req, null, next)
+  t.true(req.log.debug.calledWith({body: fixt.redactsPassword.expectedLoggedBody}, 'request body'),
+    'logs body with redacted password')
   t.true(next.called, 'calls next handler')
 })
 
