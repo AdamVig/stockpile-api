8c459300a417606c57fafc561810de81eb98863d
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-25 17:32:59 -0400

    Simplify deploy script
    - Remove bash strict mode
    - Stop disabling strict SSH key checking
    - Remove check that script is running on Travis# Please enter the commit message for your changes. Lines starting
    - Remove declarations of env vars# with '#' will be ignored, and an empty message aborts the commit.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
-IFS=$'\n\t'
-
-# Fail fast if not running on Travis
-[[ -n $TRAVIS ]] && printf "%s\n" "Must run on Travis; quitting" && exit
-
-# Declare Travis CI environment variables
-declare DEPLOY_KEY
-declare REPO_URI
-
-# Disable strict host key checking for SSH
-printf "%s\n" "Host adamvig.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-
 # Set up SSH key for access to server
 eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add <(echo "$DEPLOY_KEY")
