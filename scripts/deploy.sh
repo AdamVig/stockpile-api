72baa2d4c55726d971e6d05e2e2cf6998d8f14d7
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-25 18:50:50 -0400

    Re-enable strict SSH key checking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,14 @@
 # Expects DEPLOY_KEY to be a Base64-encoded private SSH key
 # Expects REPO_URI to be like: ssh://user@domain:/path/to/repo.git
 
+# Start the ssh agent
+eval "$(ssh-agent -s)"
+
+# Disable strict host key checking for SSH
+mkdir -p ~/.ssh
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
 # Set up SSH key for access to server
-eval "$(ssh-agent -s)" # Start the ssh agent
 ssh-add <(echo "$DEPLOY_KEY"  | base64 --decode)
 
 # Add git remote for deployment
