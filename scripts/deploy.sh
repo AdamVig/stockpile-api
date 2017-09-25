e85a7463f99b25424d857132c79fda4e371c6d24
Adam Vigneaux <adamvigneaux@gmail.com>
2017-09-25 18:42:49 -0400

    Encode deploy key with Base64
    This allows the key to be stored in a Travis env var instead of having to be an encrypted file stored in the repo.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Expects DEPLOY_KEY to be a Base64-encoded private SSH key
+# Expects REPO_URI to be like: ssh://user@domain:/path/to/repo.git
+
 # Set up SSH key for access to server
 eval "$(ssh-agent -s)" # Start the ssh agent
-ssh-add <(echo "$DEPLOY_KEY")
+ssh-add <(echo "$DEPLOY_KEY"  | base64 --decode)
 
 # Add git remote for deployment
 git remote add prod $REPO_URI
