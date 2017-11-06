a030330e8670419b8b5f408db8128476d7c38280
Adam Vigneaux <adamvigneaux@gmail.com>
2017-11-06 15:07:25 -0500

    Add local and production environments to Knex config
    This will allow running migrations on separate environments at different times.

--- knexfile.js
+++ knexfile.js
@@ -4,14 +4,39 @@ require('dotenv-safe').load({
 })
 
 module.exports = {
-  client: 'mysql',
-  connection: {
-    host: process.env.DB_URL,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+  local: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      database: 'stockpile-local',
+      user: 'root'
+    },
+    migrations: {
+      tableName: 'knexMigrations'
+    }
   },
-  migrations: {
-    tableName: 'knexMigrations'
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_URL,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      tableName: 'knexMigrations'
+    }
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.PROD_DB_URL,
+      database: process.env.PROD_DB_NAME,
+      user: process.env.PROD_DB_USER,
+      password: process.env.PROD_DB_PASSWORD
+    },
+    migrations: {
+      tableName: 'knexMigrations'
+    }
   }
 }
